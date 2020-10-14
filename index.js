f9453534be9de9b8c0fc6708da59090190ac9621
joymike <joymike@gmail.com>
2020-10-14 21:50:52 +1100

    ContextUpdate

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ let hasConversation;
 
 console.log("MP Init");
 
+Front.contextUpdates.subscribe(context => {
+  switch(context.type) {
+    case 'noConversation':
+      console.log('No conversation selected');
+      break;
+    case 'singleConversation':
+      console.log('Selected conversation:', context.conversation);
+      break;
+    case 'multiConversations':
+      console.log('Multiple conversations selected', context.conversations);
+      break;
+    default:
+      console.error(`Unsupported context type: ${context.type}`);
+      break;
+  }
+});
+
 debugger;
 
 
